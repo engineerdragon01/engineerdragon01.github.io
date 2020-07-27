d2620f7d35768be19b9ba095ef2179531b2ba669
Adam Chois <dragonboy174@gmail.com>
2020-07-26 19:45:26 -0700

    Working on filling in the modals

--- js/modal.js
+++ js/modal.js
@@ -1,27 +1,57 @@
 // Get the modal
-var modal = document.getElementById("modalOne");
+var modalOne = document.getElementById("modalOne");
+var modalTwo = document.getElementById("modalTwo");
+var modalThree = document.getElementById("modalThree");
 
-// Get the button that opens the modal
+// Get the image id that opens the modal
 var projOne = document.getElementById("projOne");
+var projTwo = document.getElementById("projTwo");
+var projThree = document.getElementById("projThree");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var spanOne = document.getElementsByClassName("close")[0];
+var spanTwo = document.getElementsByClassName("close")[1];
+var spanThree = document.getElementsByClassName("close")[2];
 
-// When the user clicks on the button, open the modal
+// When the user clicks on the image, open the modal
 projOne.onclick = function() {
-  modal.style.display = "flex";
-  modal.style.justifyContent = "center";
-  modal.style.alignContent = "center";
+  modalOne.style.display = "flex";
+  modalOne.style.justifyContent = "center";
+  modalOne.style.alignContent = "center";
+}
+
+projTwo.onclick = function() {
+  modalTwo.style.display = "flex";
+  modalTwo.style.justifyContent = "center";
+  modalTwo.style.alignContent = "center";
+}
+
+projThree.onclick = function() {
+  modalThree.style.display = "flex";
+  modalThree.style.justifyContent = "center";
+  modalThree.style.alignContent = "center";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+spanOne.onclick = function() {
+  modalOne.style.display = "none";
+}
+
+spanTwo.onclick = function() {
+  modalTwo.style.display = "none";
+}
+
+spanThree.onclick = function() {
+  modalThree.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == modalOne) {
+    modalOne.style.display = "none";
+  } else if (event.target == modalTwo) {
+    modalTwo.style.display = "none";
+  } else if (event.target == modalThree) {
+    modalThree.style.display = "none";
   }
 }
